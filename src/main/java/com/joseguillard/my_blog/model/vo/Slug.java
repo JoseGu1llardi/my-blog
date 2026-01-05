5ccde663c4a02beeb49922ba8ece715a23cfb084
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-05 20:49:48 +0000

    feat: introduce Slug value object as embeddable
    Adds a Slug value object annotated with @Embeddable to be reused
    across entities, enabling consistent and type-safe handling of
    slug values in the domain model.

---
+++ src/main/java/com/joseguillard/my_blog/model/vo/Slug.java
@@ -0,0 +1,10 @@
+package com.joseguillard.my_blog.model.vo;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public class Slug implements Serializable {
+
+}
