15ee6b4c2e8fe72c361f46e11370736349a80dc7
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-05 20:22:46 +0000

    Initial commit: Spring Boot project structure

---
+++ src/test/java/com/joseguillard/my_blog/MyPersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseguillard.my_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
