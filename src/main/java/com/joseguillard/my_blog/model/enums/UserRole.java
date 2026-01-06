14a5382a1ca8c4e4febde6aeb6121ed71a237918
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-06 20:48:28 +0000

    feat(user-role): add UserRole enum with description and authority field

---
+++ src/main/java/com/joseguillard/my_blog/model/enums/UserRole.java
@@ -0,0 +1,18 @@
+package com.joseguillard.my_blog.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("Administrator", "ROLE_ADMIN"),
+    AUTHOR("Author", "ROLE_AUTHOR"),
+    EDITOR("Editor", "ROLE_EDITOR"),;
+
+    private final String  description;
+    private final String authority;
+
+    UserRole(String description, String authority) {
+        this.description = description;
+        this.authority = authority;
+    }
+}
