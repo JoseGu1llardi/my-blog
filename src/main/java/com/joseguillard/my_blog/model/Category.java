a683e7a74a45523cf95f6f543e7646f6cd4847b9
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-17 21:14:39 +0000

    feat(category): add Category entity with JPA and Lombok annotations

---
+++ src/main/java/com/joseguillard/my_blog/model/Category.java
@@ -0,0 +1,18 @@
+package com.joseguillard.my_blog.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
