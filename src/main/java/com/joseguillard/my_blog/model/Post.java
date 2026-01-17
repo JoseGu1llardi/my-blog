b1c32d3ec7052d85c8ed0cfc2aa9ed45f86ce881
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-17 20:44:02 +0000

    feat(post): add new columns and indexes to Post entity
    - Added `slug`, `title`, `content`, `excerpt`, and `featuredImage` columns to enhance the Post entity.
    - Introduced JPA indexes for `slug`, `status`, and `publishedAt` to optimize query performance.

--- src/main/java/com/joseguillard/my_blog/model/Post.java
+++ src/main/java/com/joseguillard/my_blog/model/Post.java
@@ -1,10 +1,15 @@
 package com.joseguillard.my_blog.model;
 
+import com.joseguillard.my_blog.model.vo.Slug;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "posts")
+@Table(name = "posts", indexes = {
+        @Index(name = "idx_post_slug", columnList = "slug"),
+        @Index(name = "idx_post_status", columnList = "status"),
+        @Index(name = "idx_post_published_at", columnList = "publishedAt")
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,5 +20,22 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Embedded
+    @AttributeOverride(
+            name = "value",
+            column = @Column(name = "slug", unique = true, nullable = false))
+    private Slug slug;
+
+    @Column(nullable = false, length = 500)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(columnDefinition = "TEXT")
+    private String excerpt;
+
+    private String featuredImage;
 
 }
