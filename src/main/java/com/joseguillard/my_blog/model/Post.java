fa6070c9b2c3355f468b874d01db1d3f6f15b761
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-17 18:42:20 +0000

    feat(post): add JPA annotations and Lombok support to Post entity

--- src/main/java/com/joseguillard/my_blog/model/Post.java
+++ src/main/java/com/joseguillard/my_blog/model/Post.java
@@ -1,4 +1,19 @@
 package com.joseguillard.my_blog.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
