841478bd611742df0f3f357934d17cdbe5871287
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-19 22:27:07 +0000

    feat(category): enhance Category entity with fields, relationships, and lifecycle callbacks
    - Added `name`, `slug`, `description`, and `icon` fields with appropriate constraints.
    - Established many-to-many relationship with `Post` entity.
    - Introduced `createdDate` for timestamp tracking and `@PrePersist` callback for initialization.
    - Implemented utility method `getPostCount` to fetch the associated post-count.

--- src/main/java/com/joseguillard/my_blog/model/Category.java
+++ src/main/java/com/joseguillard/my_blog/model/Category.java
@@ -1,7 +1,12 @@
 package com.joseguillard.my_blog.model;
 
+import com.joseguillard.my_blog.model.vo.Slug;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "categories")
@@ -15,4 +20,35 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "slug", unique = true, nullable = false))
+    private Slug  slug;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String icon; // emoji or emoji's name
+
+    @ManyToMany(mappedBy = "categories")
+    @Builder.Default
+    private Set<Post> posts = new HashSet<>();
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+        if (slug == null && name != null) {
+            slug = Slug.fromTitle(name);
+        }
+    }
+
+    public int getPostCount() {
+        return posts.size();
+    }
 }
