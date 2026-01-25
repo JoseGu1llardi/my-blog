d810fe2dfa025d84b2cf5f874e9083649a338d8e
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-25 17:43:17 +0000

    feat(category): add query methods to fetch categories with posts and ordered categories
    - Implemented `findCategoriesWithPosts` to retrieve categories containing at least one post, ordered by name.
    - Added `findAllByOrderByNameAsc` to fetch all categories ordered by name.

--- src/main/java/com/joseguillard/my_blog/repository/CategoryRepository.java
+++ src/main/java/com/joseguillard/my_blog/repository/CategoryRepository.java
@@ -3,8 +3,11 @@ package com.joseguillard.my_blog.repository;
 import com.joseguillard.my_blog.model.Category;
 import com.joseguillard.my_blog.model.vo.Slug;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +18,9 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 
     boolean existsByName(String name);
+
+    @Query("SELECT c FROM Category c WHERE SIZE(c.posts) > 0 ORDER BY c.name")
+    List<Category> findCategoriesWithPosts();
+
+    List<Category> findAllByOrderByNameAsc();
 }
