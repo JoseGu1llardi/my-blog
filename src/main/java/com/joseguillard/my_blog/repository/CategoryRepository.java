cab0ee997d5fd240576e7fc13eaf79ca590b7750
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-26 20:53:52 +0000

    refactor(category): remove unused import in CategoryRepository

--- src/main/java/com/joseguillard/my_blog/repository/CategoryRepository.java
+++ src/main/java/com/joseguillard/my_blog/repository/CategoryRepository.java
@@ -4,7 +4,6 @@ import com.joseguillard.my_blog.model.Category;
 import com.joseguillard.my_blog.model.vo.Slug;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
