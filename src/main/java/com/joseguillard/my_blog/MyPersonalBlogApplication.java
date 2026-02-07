241df22dd8de6406f11363f4d40832d3f5f35af2
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-07 10:17:08 +0000

    feat(config): enable AppProperties in MyPersonalBlogApplication
    - Added `@EnableConfigurationProperties(AppProperties.class)` to initialize application-specific configuration.

--- src/main/java/com/joseguillard/my_blog/MyPersonalBlogApplication.java
+++ src/main/java/com/joseguillard/my_blog/MyPersonalBlogApplication.java
@@ -1,9 +1,12 @@
 package com.joseguillard.my_blog;
 
+import com.joseguillard.my_blog.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class MyPersonalBlogApplication {
 
 	public static void main(String[] args) {
