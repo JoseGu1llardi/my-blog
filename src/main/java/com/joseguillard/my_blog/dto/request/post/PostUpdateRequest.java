96f32dcf44479b8fedb1c5f7f0017cea7f910d06
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-08 22:47:45 +0000

    feat(dto): add PostUpdateRequest DTO
    - Introduced `PostUpdateRequest` DTO to structure post-update API requests.
    - Implemented validation rules for fields like `title`, `content`, and metadata.

---
+++ src/main/java/com/joseguillard/my_blog/dto/request/post/PostUpdateRequest.java
@@ -0,0 +1,40 @@
+package com.joseguillard.my_blog.dto.request.post;
+
+import com.joseguillard.my_blog.model.enums.PostStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostUpdateRequest {
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(max = 500, message = "Title must have a maximum of 500 characters")
+    private String title;
+
+    @NotBlank(message = "Content is mandatory")
+    private String content;
+
+    @Size(max = 1000, message = "Excerpt must have a maximum of 1000 characters")
+    private String excerpt;
+
+    private String featuredImage;
+
+    private PostStatus status;
+
+    private Set<Long> categoryIds;
+
+    @Size(max = 500, message = "Meta description must have a maximum of 500 characters")
+    private String metaDescription;
+
+    @Size(max = 500, message = "Meta keywords must have a maximum of 500 characters")
+    private String metaKeywords;
+}
