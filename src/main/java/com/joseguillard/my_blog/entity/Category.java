55c575fab02463666c73f139136fd4846570364b
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-10 21:00:58 +0000

    refactor: update `Category` package from `model` to `entity`
    - Renamed package and adjusted imports for consistency with recent package refactor efforts.

--- src/main/java/com/joseguillard/my_blog/entity/Category.java
+++ src/main/java/com/joseguillard/my_blog/entity/Category.java
@@ -1,6 +1,6 @@
-package com.joseguillard.my_blog.model;
+package com.joseguillard.my_blog.entity;
 
-import com.joseguillard.my_blog.model.vo.Slug;
+import com.joseguillard.my_blog.entity.vo.Slug;
 import jakarta.persistence.*;
 import lombok.*;
 
