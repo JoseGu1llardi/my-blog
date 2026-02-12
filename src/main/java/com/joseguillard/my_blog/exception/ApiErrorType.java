2c7b0cf7be6653386129e4a7ece0452a9b875d29
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-12 22:13:49 +0000

    feat(exception): add `ApiErrorType` enum for standardized error categorization

---
+++ src/main/java/com/joseguillard/my_blog/exception/ApiErrorType.java
@@ -0,0 +1,10 @@
+package com.joseguillard.my_blog.exception;
+
+public enum ApiErrorType {
+    RESOURCE_NOT_FOUND,
+    INVALID_EMAIL,
+    INVALID_SLUG,
+    DUPLICATED_RESOURCE,
+    VALIDATION_ERROR,
+    INTERNAL_ERROR
+}
