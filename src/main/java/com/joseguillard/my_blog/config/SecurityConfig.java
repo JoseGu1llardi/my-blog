ea9b3d8438e28ac8cb2cd1dc1158e7002012fceb
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-13 22:34:39 +0000

    feat(config): add `SecurityConfig` with basic security setup
    - Configured `HttpSecurity` to disable CSRF and frame options for H2 console access.
    - Added a `BCryptPasswordEncoder` bean for password encryption.

---
+++ src/main/java/com/joseguillard/my_blog/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.joseguillard.my_blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Configures request authorization and CSRF/header options
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
