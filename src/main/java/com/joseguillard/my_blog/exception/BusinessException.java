eed4ceef89843b3719b4afbacf22cc3f14aaa146
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-14 16:00:42 +0000

    feat(exception): add `BusinessException` for custom runtime exceptions

---
+++ src/main/java/com/joseguillard/my_blog/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.joseguillard.my_blog.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
