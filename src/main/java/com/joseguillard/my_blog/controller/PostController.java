b888bf00e6b89c371e9895aa8aae6efa620c8595
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-17 20:39:59 +0000

    feat(controller): add `getPostsByAuthor` endpoint and update `getPostsByCategory` method
    - Introduced `getPostsByAuthor` method to enable filtering posts by author.
    - Renamed and refined `getPostByCategory` to `getPostsByCategory` for consistency.

--- src/main/java/com/joseguillard/my_blog/controller/PostController.java
+++ src/main/java/com/joseguillard/my_blog/controller/PostController.java
@@ -58,13 +58,25 @@ public class PostController {
     }
 
     @GetMapping("/category/{slug}")
-    public ResponseEntity<ApiResponse<PageResponse<PostSummaryResponse>>> getPostByCategory(
+    public ResponseEntity<ApiResponse<PageResponse<PostSummaryResponse>>> getPostsByCategory(
             @PathVariable String slug,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
         Pageable pageable = PageRequest.of(page, size);
         Page<PostSummaryResponse> response = postService.getPostsByCategory(slug, pageable);
+
+        return ResponseEntity.ok(ApiResponse.success(PageResponse.of(response)));
+    }
+
+    @GetMapping("/author/{slug}")
+    public ResponseEntity<ApiResponse<PageResponse<PostSummaryResponse>>> getPostsByAuthor(
+            @PathVariable String slug,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PostSummaryResponse> response = postService.getPostByAuthor(slug, pageable);
 
         return ResponseEntity.ok(ApiResponse.success(PageResponse.of(response)));
     }
