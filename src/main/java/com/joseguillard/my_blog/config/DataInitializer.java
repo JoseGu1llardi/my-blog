b3420baacd1e57b5422c1f64a552738817e0f96c
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-21 15:00:46 +0000

    feat(data): initialize categories and associate them with posts
    - Added `CategoryRepository` for managing categories.
    - Initialized sample categories during data setup.
    - Associated posts with relevant categories.

--- src/main/java/com/joseguillard/my_blog/config/DataInitializer.java
+++ src/main/java/com/joseguillard/my_blog/config/DataInitializer.java
@@ -1,12 +1,14 @@
 package com.joseguillard.my_blog.config;
 
 import com.joseguillard.my_blog.entity.Author;
+import com.joseguillard.my_blog.entity.Category;
 import com.joseguillard.my_blog.entity.Post;
 import com.joseguillard.my_blog.entity.enums.PostStatus;
 import com.joseguillard.my_blog.entity.enums.UserRole;
 import com.joseguillard.my_blog.entity.vo.Email;
 import com.joseguillard.my_blog.entity.vo.Slug;
 import com.joseguillard.my_blog.repository.AuthorRepository;
+import com.joseguillard.my_blog.repository.CategoryRepository;
 import com.joseguillard.my_blog.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 @Component
@@ -25,6 +28,8 @@ public class DataInitializer implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final PostRepository postRepository;
+    private final CategoryRepository categoryRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     /**
@@ -50,6 +55,20 @@ public class DataInitializer implements CommandLineRunner {
 
         authorRepository.save(author);
 
+        Category category1 = Category.builder()
+                .name("Java and Spring")
+                .description("Posts related with Java and Spring Boot applications")
+                .icon("☕, \uD83C\uDF31")
+                .build();
+
+        Category category2 = Category.builder()
+                .name("Hibernate")
+                .description("Discovering the magics of ORM")
+                .icon("\uD83D\uDC18")
+                .build();
+
+        categoryRepository.saveAll(List.of(category1, category2));
+
         // Builds post with metadata and content
         Post post1 = Post.builder()
                 .title("Java and Spring Boot")
@@ -57,10 +76,10 @@ public class DataInitializer implements CommandLineRunner {
                 .content("Content of the first post about Java and Spring Boot...")
                 .excerpt("An Introduction to Java and Spring Boot Development")
                 .status(PostStatus.PUBLISHED)
+                .categories(new HashSet<>(List.of(category1,  category2)))
                 .publishedAt(LocalDateTime.now())
                 .viewsCount(0)
                 .author(author)
-                .categories(Collections.emptySet())
                 .build();
 
         // Builds the second published post with the current timestamp
@@ -70,10 +89,10 @@ public class DataInitializer implements CommandLineRunner {
                 .content("Post content about dirty checking...")
                 .excerpt("Understand how Hibernate automatically detects changes")
                 .status(PostStatus.PUBLISHED)
+                .categories(new HashSet<>(List.of(category2)))
                 .publishedAt(LocalDateTime.now())
                 .viewsCount(0)
                 .author(author)
-                .categories(Collections.emptySet())
                 .build();
 
         postRepository.saveAll(List.of(post1, post2));
