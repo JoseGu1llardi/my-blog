83d4b1a9ca3e94a55d70d131c661205d589411c4
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-22 11:38:02 +0000

    feat(controller): add `getAuthorBySlug` endpoint
    - Introduced `/authors/{slug}` endpoint in `AuthorController` to fetch an author by their slug.
    - Added `@PathVariable` annotation to handle slug-based requests.

--- src/main/java/com/joseguillard/my_blog/controller/AuthorController.java
+++ src/main/java/com/joseguillard/my_blog/controller/AuthorController.java
@@ -7,6 +7,7 @@ import com.joseguillard.my_blog.service.AuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +32,12 @@ public class AuthorController {
         List<AuthorSummaryResponse> authors = authorService.findAuthorWithPosts();
 
         return ResponseEntity.ok(ApiResponse.success(authors));
+    }
+
+    @GetMapping("/{slug}")
+    public ResponseEntity<ApiResponse<AuthorResponse>> getAuthorBySlug(
+            @PathVariable String slug) {
+        AuthorResponse author = authorService.findBySlug(slug);
+        return ResponseEntity.ok(ApiResponse.success(author));
     }
 }
