e4fd36c9b9ad2f776fbe0f77ffe1ecedac227c76
JoseGu1llardi <junior11_junior@hotmail.com>
2026-02-23 19:36:29 +0000

    feat(config): add global CORS configuration
    - Introduced `CorsConfig` class to define global CORS settings.
    - Configured allowed origins, methods, headers, and credentials for `/api/v1/**` endpoints.

---
+++ src/main/java/com/joseguillard/my_blog/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.joseguillard.my_blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter  corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config =  new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "https://*.mydomain.com"
+        ));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/api/v1/**", config);
+
+        return new CorsFilter(source);
+    }
+}
