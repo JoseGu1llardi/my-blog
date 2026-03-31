07448c5973971ba6e7ef961ebebae5f108cda8e2
Jose Wellington Ribeiro <junior11_junior@hotmail.com>
2026-03-31 14:01:37 +0100

    feat(staging): add staging environment support
    - Add `DataInitializerStaging` to prepopulate database with a default admin user in staging profile
    - Create `docker-compose.staging.yml` for staging-specific services configuration
    - Update `.gitignore` to exclude staging-related and local config files

---
+++ src/main/java/com/joseguillard/my_blog/config/DataInitializerStaging.java
@@ -0,0 +1,40 @@
+package com.joseguillard.my_blog.config;
+
+import com.joseguillard.my_blog.entity.Author;
+import com.joseguillard.my_blog.entity.enums.UserRole;
+import com.joseguillard.my_blog.entity.vo.Email;
+import com.joseguillard.my_blog.repository.AuthorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("staging")
+@RequiredArgsConstructor
+public class DataInitializerStaging implements CommandLineRunner {
+
+    @Value("${app.init.password}")
+    private String authorPassword;
+
+    private final AuthorRepository authorRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (authorRepository.count() > 0) return;
+
+        Author author = Author.builder()
+                .userName("gr1llard")
+                .email(Email.of("jwribeiro.dev@gmail.com"))
+                .password(passwordEncoder.encode(authorPassword))
+                .fullName("Jose Wellington")
+                .role(UserRole.ADMIN)
+                .active(true)
+                .build();
+
+        authorRepository.save(author);
+    }
+}
