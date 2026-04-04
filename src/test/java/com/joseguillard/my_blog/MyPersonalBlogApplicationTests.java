33353c3a533224a13587849768542e7705f60a17
Jose Wellington Ribeiro <junior11_junior@hotmail.com>
2026-04-04 16:37:39 +0100

    test(config): set active profile to "test" in MyPersonalBlogApplicationTests

--- src/test/java/com/joseguillard/my_blog/MyPersonalBlogApplicationTests.java
+++ src/test/java/com/joseguillard/my_blog/MyPersonalBlogApplicationTests.java
@@ -2,8 +2,10 @@ package com.joseguillard.my_blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyPersonalBlogApplicationTests {
 
 	@Test
