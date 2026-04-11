1300db2b838e6dfef5c1046db2347011a8619e0b
Jose Wellington Ribeiro <junior11_junior@hotmail.com>
2026-04-11 12:08:49 +0100

    refactor: remove unused AuthorService methods and repository queries
    - Delete findAuthorByEmail, findAuthorByUsername, isUsernameExists, isEmailExists
    - Delete findByEmail, existsByUserName, existsByEmail from AuthorRepository
    - No controller callers existed - unaudited API surface removed

--- src/main/java/com/joseguillard/my_blog/repository/AuthorRepository.java
+++ src/main/java/com/joseguillard/my_blog/repository/AuthorRepository.java
@@ -1,7 +1,6 @@
 package com.joseguillard.my_blog.repository;
 
 import com.joseguillard.my_blog.entity.Author;
-import com.joseguillard.my_blog.entity.vo.Email;
 import com.joseguillard.my_blog.entity.vo.Slug;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,12 +16,6 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     Optional<Author> findBySlug(Slug slug);
 
     Optional<Author> findByUserName(String userName);
-
-    Optional<Author> findByEmail(Email email);
-
-    boolean existsByUserName(String userName);
-
-    boolean existsByEmail(Email email);
 
     List<Author> findByActiveTrue();
 
