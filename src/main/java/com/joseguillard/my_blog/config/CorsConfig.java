72bb3bdc84e106872cdb55999906f09149e53d4b
JoseGu1llardi <junior11_junior@hotmail.com>
2026-05-05 21:58:57 +0100

    chore(config): update CORS allowed origins property type to List

--- src/main/java/com/joseguillard/my_blog/config/CorsConfig.java
+++ src/main/java/com/joseguillard/my_blog/config/CorsConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins[0]}")
+    @Value("${app.cors.allowed-origins}")
     private List<String> allowedOrigins;
 
     @Bean
